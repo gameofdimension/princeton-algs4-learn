2778596f9e7801c2a0442931acde91f3692282ec
yzq <yzq@leyantech.com>
2020-08-11 14:13:45 +0800

    fix bug

--- collinear-points/src/main/java/FastCollinearPoints.java
+++ collinear-points/src/main/java/FastCollinearPoints.java
@@ -52,18 +52,14 @@ public class FastCollinearPoints {
   }   // finds all line segments containing 4 or more points
 
   /**
-   * 按照 spec 上的思路，有一个很重要的问题是去重。k 点共线的直线可能会被加入 k 次。
-   * 普遍的去重思路有两种：
-   * 一是在结果集新增过程中去重，即发现是重复的则不加入。
-   * 二是最后整体sort and unique 去重。
-   * 但是根据 faq 中的最后部分，n 个点的集合大于 4 个点共线的 unique 直线数至少有 O(n^2) 规模，
-   * 于是思路二内存使用会超标。思路一简单的 for loop 查重，单次耗时是线性 O(n^2) 的，肯定时间性能会超标。
-   * 如果使用高效的平衡树存储新增直线，单次查重以及插入的性能是 O(log(n^2)) ,看起来是满足要求的，但是这类
-   * 高级数据结构相对于本节课显然是超纲了的。
+   * 按照 spec 上的思路，有一个很重要的问题是去重。k 点共线的直线可能会被加入 k 次。 普遍的去重思路有两种： 一是在结果集新增过程中去重，即发现是重复的则不加入。 二是最后整体sort
+   * and unique 去重。 但是根据 faq 中的最后部分，n 个点的集合大于 4 个点共线的 unique 直线数至少有 O(n^2) 规模， 于是思路二内存使用会超标。思路一简单的
+   * for loop 查重，单次耗时是线性 O(n^2) 的，肯定时间性能会超标。 如果使用高效的平衡树存储新增直线，单次查重以及插入的性能是 O(log(n^2))
+   * ,看起来是满足要求的，但是这类 高级数据结构相对于本节课显然是超纲了的。
    *
-   * Coursera 课程 forum 上有人给出了非常有洞见的解决方法。核心是观察到在 k 点共线时，以不同的点作为 origin ，
-   * 虽然会得到 k 个相同的结果，这是这 k 种结果是可以区分的，区分的方法则是观察作为 origin 的点在直线上的相对
-   * 位置，是第一个点还是其他位置。这种位置关系可以用排序的方法确定。
+   * Coursera 课程 forum 上有人给出了非常有洞见的解决方法。核心是观察到在 k 点共线时，以不同的点作为 origin ， 虽然会得到 k 个相同的结果，这是这 k
+   * 种结果是可以区分的，区分的方法则是观察作为 origin 的点在直线上的相对 位置，是第一个点还是其他位置。这种位置关系可以用排序的方法确定。
+   *
    * @return list
    */
   private List<LineSegment> computeSegments() {
@@ -72,42 +68,63 @@ public class FastCollinearPoints {
       return tmp;
     }
 
-    List<MySegment> myList = new ArrayList<MySegment>();
     for (int i = 0; i < points.length; i++) {
-      Point[] tmpArr = points.clone();
-      Point origin = points[i];
+      Point[] tmpArr = new Point[points.length];
+      System.arraycopy(points, 0, tmpArr, 0, points.length);
+      final Point origin = points[i];
+
+      // System.out.println("before sort");
+      // for (Point pp : tmpArr) {
+      //   System.out.println(pp);
+      // }
       Arrays.sort(tmpArr, origin.slopeOrder());
+      // System.out.println("after sort");
+      // for (Point pp : tmpArr) {
+      //   System.out.println(pp);
+      // }
       int begin = 1;
       double slope1 = origin.slopeTo(tmpArr[begin]);
       int end = 1;
       while (end < tmpArr.length) {
+        // System.out.println(end);
         double slope2 = origin.slopeTo(tmpArr[end]);
+        // System.out.println(slope1 + ":" + slope2);
         if (Double.compare(slope1, slope2) == 0) {
           end += 1;
         } else {
-          int len = end - begin + 1;
+          // System.out.println(begin + " vs " + end);
+          int len = end - begin;
+          // System.out.println(111);
           if (len >= 3) {
-            tmpArr[begin - 1] = origin;
-            Arrays.sort(tmpArr, begin - 1, end + 1);
-            myList.add(new MySegment(tmpArr[begin - 1], tmpArr[end]));
+            Point[] again = new Point[len+1];
+            // System.out.println(111);
+            System.arraycopy(tmpArr, begin, again, 0, len);
+            // System.out.println(2222);
+            again[len] = origin;
+
+            // System.out.println(3333);
+            Arrays.sort(again);
+            // System.out.println(4444);
+            // System.out.println("origin:" + origin);
+            // System.out.println(again[0]);
+
+            // System.out.println();
+            // for (Point pp : again) {
+            //   System.out.println("000:" + pp);
+            // }
+            if (origin.compareTo(again[0]) == 0) {
+              // System.out.println(5555);
+              tmp.add(new LineSegment(again[0], again[len]));
+            }
+            // System.out.println(6666);
           }
           begin = end;
           slope1 = origin.slopeTo(tmpArr[begin]);
+          end += 1;
         }
       }
     }
-    MySegment[] mySegments = myList.toArray(new MySegment[0]);
-    Arrays.sort(mySegments);
-
-    MySegment previous = mySegments[0];
-    tmp.add(new LineSegment(previous.p, previous.q));
-    for (int i = 1; i < mySegments.length; i++) {
-      if (mySegments[i].compareTo(previous) == 0) {
-        continue;
-      }
-      previous = mySegments[i];
-      tmp.add(new LineSegment(previous.p, previous.q));
-    }
+    // System.out.println(7777);
     return tmp;
   }
 
